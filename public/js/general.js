3fbccdea25c46194d2ba9ff09b6c1cdf45211a79
ThibaudLafont <thibaudlafont@gmail.com>
2018-11-02 15:34:19 +0100

    Start to build dropdown menu

--- public/js/general.js
+++ public/js/general.js
@@ -13,5 +13,30 @@ $('#topheader-mobile img').click(function(){
     }
 });
 
+// Dropdown menus
+
+// Set ul width
+function setUlMenuWidth($span, $ul)
+{
+    var width = $($span).outerWidth();
+    $($ul).css('width', width);
+
+}
+setUlMenuWidth('.project-link a[href="/projects/level-design"] span', '#ld-dropdown');
+setUlMenuWidth('.project-link a[href="/projects/game-design"] span', '#gd-dropdown');
+// Show/Hide
+    // LD menu
+$(' .project-link a[href="/projects/level-design"] ').hover(function(){
+    $('#ld-dropdown').show()
+}, function(){
+    $('#ld-dropdown').hide()
+})
+// GD menu
+$(' .project-link a[href="/projects/game-design"] ').hover(function(){
+    $('#gd-dropdown').show()
+}, function(){
+    $('#gd-dropdown').hide()
+})
+
 // Detect and put a timer on flash-messages for hide them
 $('.flash').delay(5000).fadeOut( 500 );
